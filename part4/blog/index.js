5c286657f40c0c28f37acb4a2d51f69c0baed329
kong lingyu <lionyu_kong@outlook.com>
2025-08-22 01:00:39 +0200

    models/blog.js

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -3,15 +3,8 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 const app = express()
+const Blog = require('./models/blog')
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
