573f91ce037dba03e4213740c0b3b8e417bca608
kong lingyu <lionyu_kong@outlook.com>
2025-08-31 00:01:23 +0200

    connect

--- part4/blog/controllers/blog.js
+++ part4/blog/controllers/blog.js
@@ -1,6 +1,7 @@
 const blogRouter = require('express').Router()
 
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 // blogRouter.get('/', (request, response) => {
 //   Phonebook.find({}).then(phonebooks => {
@@ -25,12 +26,30 @@ blogRouter.get('/:id', async (request, response, next) => {
 })
 
 blogRouter.post('/', async (request, response, next) => {
-  const blog = new Blog(request.body)
+  const body = request.body
+  const user = await User.findById(body.userId)
+  if (!user) {
+    return response.status(400).json({ error: 'userId missing or not valid' })
+  }
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes === undefined ? 0 : body.likes,
+    user: user._id
+  })
 
-  savedBlog = await blog.save()
+  try {
+    savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+    response.status(201).json(savedBlog)
+  } catch (error) {
+    next(error)
+  }
 
-  response.status(201).json(savedBlog)
-    .catch(error => next(error))
+
+
 })
 
 blogRouter.delete('/:id', async (request, response, next) => {
