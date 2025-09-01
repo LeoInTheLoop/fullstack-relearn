7ac0057c8b05bc5aa974ca60eec6b5c30bd17fa8
kong lingyu <lionyu_kong@outlook.com>
2025-09-02 01:06:15 +0200

    fix test

--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -28,6 +28,8 @@ beforeEach(async () => {
     let blogObject = new blogModel(blog)
     await blogObject.save()
   }
+
+
   logger.info("save done")
 
 })
@@ -52,107 +54,6 @@ test('a specific title is within the returned blog', async () => {
   const titles = response.body.map(e => e.title)
   // logger.info(titles)
   assert.strictEqual(titles.includes('test2'), true)
-})
-
-describe('addition of a new note', () => {
-  test('blog without title is not added', async () => {
-    const newblog = {
-      "title": "5",
-      "author": "String ba;a;",
-      "url": " www.test2.com",
-      "likes": 13
-    }
-
-    await api
-      .post('/api/blogs')
-      .send(newblog)
-      .expect(400)
-
-
-    const blogAtEnd = await helper.blogsInDb()
-
-
-    assert.strictEqual(blogAtEnd.length, helper.initialBlog.length)
-  })
-
-  test('a valid note can be added HTTP POST', async () => {
-    const newBlog = {
-      "title": "test4",
-      "author": "String ba;a;",
-      "url": " www.test2.com",
-      "likes": 13
-    }
-
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
-
-    const blogAtEnd = await helper.blogsInDb()
-
-    const contents = blogAtEnd.map(n => n.title)
-    assert.strictEqual(blogAtEnd.length, helper.initialBlog.length + 1)
-
-    assert(contents.includes('test4'))
-  })
-})
-// test('a specific note can be viewed', async () => {
-//   const blogsAtStart = await helper.blogsInDb()
-//   const noteToView = blogsAtStart[0]
-
-//   const resultNote = await api
-//     .get(`/api/blogs/${noteToView.id}`)
-//     .expect(200)
-//     .expect('Content-Type', /application\/json/)
-
-//   assert.deepStrictEqual(resultNote.body, noteToView)
-// })
-test('a note have id property', async () => {
-  const blogsAtStart = await helper.blogsInDb()
-  const noteToView = blogsAtStart[0]
-  // logger.info(noteToView)
-  assert.ok(noteToView.id)
-})
-test('note like default 0 ', async () => {
-  const newBlog = {
-    "title": "test4",
-    "author": "String ba;a;",
-    "url": " www.test2.com",
-  }
-
-  const response = await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  assert.strictEqual(response.body.likes, 0)
-})
-
-test.only('a note can be update like', async () => {
-  const blogs = await helper.blogsInDb()
-  const blogToUpdate = blogs.find(b => b.title === 'test2')
-  // logger.info(blogToDelete)    
-  await api
-    .put(`/api/blogs/${blogToUpdate.id}`)
-    .send({ likes: 20 })
-
-  const blogsAtEnd = await helper.blogsInDb()
-  const updatedBlog = blogsAtEnd.find(b => b.title === 'test2')
-  assert.strictEqual(updatedBlog.likes, 20)
-
-})
-
-test('a note can be deleted', async () => {
-  const blogsAtStart = await helper.blogsInDb()
-  const blogToDelete = blogsAtStart[0]
-  // logger.info(blogToDelete)    
-  await api
-    .delete(`/api/blogs/${blogToDelete.id}`)
-    .expect(204)
-  const blogsAtEnd = await helper.blogsInDb()
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlog.length - 1)
 })
 
 // user tests
@@ -234,9 +135,9 @@ describe('when there is initially one user in db', () => {
     const usersAtStart = await helper.usersInDb()
 
     const newUser = {
-      username: 'sh',
-      name: 'shorterpassword',
-      password: 'saadsfga',
+      "username": "sh",
+      "name": "shorterpassword",
+      "password": "saadsfga",
     }
 
     const result = await api
@@ -246,11 +147,127 @@ describe('when there is initially one user in db', () => {
       .expect('Content-Type', /application\/json/)
 
     const usersAtEnd = await helper.usersInDb()
-    assert(result.body.error.includes('password is required and must be at least 3 characters long'))
+    assert(result.body.error.includes('Path `username` (`sh`) is shorter than the minimum allowed length (3)'))
 
     assert.strictEqual(usersAtEnd.length, usersAtStart.length)
   })
 })
+
+
+describe('addition of a new blog', () => {
+  test('blog without title is not added', async () => {
+
+    const users = await helper.usersInDb()
+    const savedUser = users[0]
+    // logger.info(saved
+    const newblog = {
+      "title": "5",
+      "author": "String ba;a;",
+      "url": " www.test2.com",
+      "likes": 13,
+      "userId":savedUser._id
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newblog)
+      .expect(400)
+
+
+    const blogAtEnd = await helper.blogsInDb()
+
+
+    assert.strictEqual(blogAtEnd.length, helper.initialBlog.length)
+  })
+
+  test('a valid note can be added HTTP POST', async () => {
+    const users = await helper.usersInDb()
+    const savedUser = users[0]
+    const newBlog = {
+      "title": "test4",
+      "author": "String ba;a;",
+      "url": " www.test2.com",
+      "likes": 13,
+      "userId":savedUser.id
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogAtEnd = await helper.blogsInDb()
+
+    const contents = blogAtEnd.map(n => n.title)
+    assert.strictEqual(blogAtEnd.length, helper.initialBlog.length + 1)
+
+    assert(contents.includes('test4'))
+  })
+})
+// test('a specific note can be viewed', async () => {
+//   const blogsAtStart = await helper.blogsInDb()
+//   const noteToView = blogsAtStart[0]
+
+//   const resultNote = await api
+//     .get(`/api/blogs/${noteToView.id}`)
+//     .expect(200)
+//     .expect('Content-Type', /application\/json/)
+
+//   assert.deepStrictEqual(resultNote.body, noteToView)
+// })
+test('a note have id property', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+  const noteToView = blogsAtStart[0]
+  // logger.info(noteToView)
+  assert.ok(noteToView.id)
+  
+})
+test('note like default 0 ', async () => {
+      const users = await helper.usersInDb()
+    const savedUser = users[0]
+  const newBlog = {
+    "title": "test4",
+    "author": "String ba;a;",
+    "url": " www.test2.com",
+    "userId":savedUser.id
+  }
+
+  const response = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  assert.strictEqual(response.body.likes, 0)
+})
+
+test.only('a note can be update like', async () => {
+  const blogs = await helper.blogsInDb()
+  const blogToUpdate = blogs.find(b => b.title === 'test2')
+  // logger.info(blogToDelete)    
+  await api
+    .put(`/api/blogs/${blogToUpdate.id}`)
+    .send({ likes: 20 })
+
+  const blogsAtEnd = await helper.blogsInDb()
+  const updatedBlog = blogsAtEnd.find(b => b.title === 'test2')
+  assert.strictEqual(updatedBlog.likes, 20)
+
+})
+
+test('a note can be deleted', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+  const blogToDelete = blogsAtStart[0]
+  // logger.info(blogToDelete)    
+  await api
+    .delete(`/api/blogs/${blogToDelete.id}`)
+    .expect(204)
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlog.length - 1)
+})
+
+
 
 
 
